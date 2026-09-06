8ea176de80e035b5d2e050db7d188a815a881c9a
ghydrangea <kittituch14@gmail.com>
2026-09-06 14:04:27 +0700

    feat: remove Pages setup step from GitHub Actions workflow and ensure basePath is set in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: process.env.GITHUB_ACTIONS ? "/retirement-plan" : undefined,
   allowedDevOrigins: ["localhost", "127.0.0.1", "172.17.96.1"],
 };
 
